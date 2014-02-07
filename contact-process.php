f41d35e0ea48438e89a9d9dcaf701513d05acbe9
David Saunders <djsaun@gmail.com>
2014-02-07 17:13:13 -0600

    functional contact form created

---
+++ contact-process.php
@@ -0,0 +1,16 @@
+<?php 
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+$email_body = "";
+$email_body = $email_body . "Name: " . $name . "\n";
+$email_body = $email_body . "Email: " . $email . "\n";
+$email_body = $email_body . "Message: " . $message;
+// echo $email_body;
+
+// TODO: Send Email
+
+header("Location: contact-thanks.php"); //redirects to page after email is sent
+
+?>
